69f9fe190fc84db35ff08c61dca9055361d437a1
Jelle <jelle@mx3d.com>
2022-09-20 09:59:38 +0200

    First couple of exercises

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -5,7 +5,7 @@
 // I AM NOT DONE
 
 fn main() {
-    let a = ???
+    let a = "Create an array with at least 100 elements in it where the ??? is.Create an array with at least 100 elements in it where the ??? is.";
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
@@ -13,3 +13,4 @@ fn main() {
         println!("Meh, I eat arrays like that for breakfast.");
     }
 }
+["Are we there yet?"; 1001]
